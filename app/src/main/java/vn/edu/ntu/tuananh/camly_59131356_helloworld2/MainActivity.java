7682421367ec509aff463ce95020740b263bf845
chaucamly <ly.cc.59cntt@ntu.edu.vn>
2020-04-08 21:50:50 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/tuananh/camly_59131356_helloworld2/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.tuananh.camly_59131356_helloworld2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
